d4fd5f7acccb4aeeffbbd0221e2464bf376f9d37
Shamaoke <shamaoke@yandex.ru>
2024-05-29 11:17:11 +0300

    [feature] Implement printing notification when the program is about to exit

--- main.cpp
+++ main.cpp
@@ -16,6 +16,10 @@ auto main(int argc, char* argv[ ], [[maybe_unused]] char* env[ ]) -> int {
 
   engine.load(window);
 
+  auto notify_quit { [ ]( ) { qInfo() << "Exiting from the Sample media player"; } };
+
+  QObject::connect(&app, &QCoreApplication::aboutToQuit, notify_quit);
+
   auto exit_status { app.exec() };
 
   return exit_status;
