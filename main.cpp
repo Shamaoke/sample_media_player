bf6deb88d7ed8ff3c076bf1087ada3c1b8b832a0
Shamaoke <shamaoke@yandex.ru>
2024-05-29 09:56:51 +0300

    [issues] Resove the issue with importing QML components

--- main.cpp
+++ main.cpp
@@ -10,8 +10,9 @@ auto main(int argc, char* argv[ ], [[maybe_unused]] char* env[ ]) -> int {
 
   auto app { QGuiApplication { argc, argv, } };
   auto engine { QQmlApplicationEngine { } };
-  // auto window { QUrl { "qrc:/Components/Main.qml" } };
-  auto window { QUrl { "qrc:/qt/qml/Components/Main.qml" } };
+  auto window { QUrl { "qrc:/Components/Main.qml" } };
+
+  engine.addImportPath("qrc:/");
 
   engine.load(window);
 
