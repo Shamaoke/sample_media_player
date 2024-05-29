d26653273de35eb643d154eb8a80b840f0ebd695
Shamaoke <shamaoke@yandex.ru>
2024-05-29 08:51:50 +0300

    [build] Edit CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ auto main(int argc, char* argv[ ], [[maybe_unused]] char* env[ ]) -> int {
 
   auto app { QGuiApplication { argc, argv, } };
   auto engine { QQmlApplicationEngine { } };
-  auto window { QUrl { "qrc:/Components/Main.qml" } };
+  // auto window { QUrl { "qrc:/Components/Main.qml" } };
+  auto window { QUrl { "qrc:/qt/qml/Components/Main.qml" } };
 
   engine.load(window);
 
