31ffc18a76f06e55055b098c8f64e5836cd92d5a
Shamaoke <shamaoke@yandex.ru>
2024-05-31 10:39:04 +0300

    [refactoring] Edit main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+namespace Callbacks {
+  auto notify_quit { [ ]( ) { qInfo() << "Exiting from the Sample media player"; } };
+}
+
 auto main(int argc, char* argv[ ], [[maybe_unused]] char* env[ ]) -> int {
 
   auto app { QGuiApplication { argc, argv, } };
@@ -16,9 +20,7 @@ auto main(int argc, char* argv[ ], [[maybe_unused]] char* env[ ]) -> int {
 
   engine.load(window);
 
-  auto notify_quit { [ ]( ) { qInfo() << "Exiting from the Sample media player"; } };
-
-  QObject::connect(&app, &QCoreApplication::aboutToQuit, notify_quit);
+  QObject::connect(&app, &QCoreApplication::aboutToQuit, Callbacks::notify_quit);
 
   auto exit_status { app.exec() };
 
